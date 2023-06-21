744722ecdcadc34ae7738dbd687b6e8304aefe36
Josef Zacek <joezacek@gmail.com>
2023-06-21 11:53:36 +0100

    add click to copy functionality

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ $('input.user-input').on('input', function(e) {
     $(".txt-one").val(numbered.stringify($(this).val()));
   }
 });
+// click to copy
+$(".copy-text-one-button").click(function() {
+  copyText("txt-one")
+})
+
+$(".copy-text-two-button").click(function() {
+  copyText("txt-two")
+})
+
+function copyText(className) {
+  $("textarea." + className).select();
+  document.execCommand('copy');
+}
