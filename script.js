8169b5b76ade4f00957bae969c132645f5547a6a
Josef Zacek <joezacek@gmail.com>
2023-07-04 14:22:23 +0100

    add js for number to text functionality

--- script.js
+++ script.js
@@ -6,6 +6,63 @@ $('input.user-input').on('input', function(e) {
     $(".txt-one").val(numbered.stringify($(this).val()));
   }
 });
+
+// number to text
+$('input.user-input').on('input', function(e) {
+  $(".txt-two").val("");
+  var userInput = "";
+  userInput = $(this).val();
+
+  if (userInput.length > 0 ){
+    $("button").removeClass("disabled")
+  } else {
+    $("button").addClass("disabled")
+  }
+
+  var number = ""
+  for (var i = 0; i < userInput.length; i++) {
+    number += convertToWord(userInput[i])
+    number += " ";
+  }
+
+  $(".txt-two").val(txt.trim());
+
+  function convertToWord(number) {
+    switch (number) {
+      case '0':
+        return "zero"
+        break;
+      case '1':
+        return "one"
+        break;
+      case '2':
+        return "two"
+        break;
+      case '3':
+        return "three"
+        break;
+      case '4':
+        return "four"
+        break;
+      case '5':
+        return "five"
+        break;
+      case '6':
+        return "six"
+        break;
+      case '7':
+        return "seven"
+        break;
+      case '8':
+        return "eight"
+        break;
+      case '9':
+        return "nine"
+        break;
+    }
+  }
+});
+
 // click to copy
 $(".copy-text-one-button").click(function() {
   copyText("txt-one")
